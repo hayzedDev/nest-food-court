46ef35200871e154956fc35a986120c17fdb3c21
Azeez <azeezade04@gmail.com>
2023-01-30 21:39:25 +0100

    Chore: init

--- dist/src/dtos/addonsdto/update-addon.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateAddonDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const create_addon_dto_1 = require("./create-addon.dto");
-class UpdateAddonDto extends (0, mapped_types_1.PartialType)(create_addon_dto_1.CreateAddonDto) {
-}
-exports.UpdateAddonDto = UpdateAddonDto;
