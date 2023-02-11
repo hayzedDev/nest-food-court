46aa1b30f86a5b14243992771ec0651d7798d2e8
Azeez <azeezade04@gmail.com>
2023-02-11 22:22:42 +0100

    Added user functionalities

---
+++ database/migrations/20230209114331_update_user_table.js
@@ -0,0 +1,20 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('users', function (table) {
+    table
+      .integer('brandId')
+      .references('id')
+      .inTable('brands')
+      .onDelete('CASCADE')
+      .onUpdate('CASCADE');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
