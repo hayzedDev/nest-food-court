a8dbd7cf2afc91b951942114a9ef2151dc7280a5
Azeez <azeezade04@gmail.com>
2023-02-25 13:23:34 +0100

    added another commit

---
+++ database/migrations/20230225121523_update_user_table_2.js
@@ -0,0 +1,21 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.alterTable('users', function (table) {
+    table.string('username');
+    table.string('password');
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.alterTable('users', function (table) {
+    table.dropColumn('username');
+    table.dropColumn('password');
+  });
+};
